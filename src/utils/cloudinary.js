4c64f4ad7875b142ec19e813854e54a14b34b42e
Your Name <rishabhyadav70070@gmail.com>
2025-03-13 15:35:49 +0530

    register controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -14,8 +14,9 @@ const uploadOnCloudinary = async (localFilePath) => {
         const response = await cloudinary.uploader.upload
         (localFilePath, { resourse_type: "auto" });
         //file has been uploaded successfully
-        console.log("file is uploaded on cloudinary",
-            response.url);
+        // console.log("file is uploaded on cloudinary",
+        //     response.url);
+            fs.unlinkSync(localFilePath); // remove temperarly saved file from local storage
             return response;
         // Remove file from local storage
         
